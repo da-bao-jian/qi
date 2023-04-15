2f4d4d5330fa495f442c6a175824f42ecda96070
HarryG <gaohaoyuandd@gmail.com>
2023-04-15 18:48:43 -0400

    added batch sub router

--- Auction.sol
+++ Auction.sol
@@ -4,11 +4,21 @@ pragma solidity ^0.8.19;
 import "./ERC1271.sol";
 import "./Vault.sol";
 
+// Add the IRouter interface later
+// will handke how to execute all the calldata
+// also can enable partial bundle execution
+interface IRouter {
+    function execute(bytes32 calldataHash) external;
+}
+
 contract Auction {
     ERC1271 private erc1271;
     uint256 private constant auctionDuration = 10; // 10 blocks
 
     Vault private vault;
+
+    event AuctionCompleted(uint256 winningIndex, TransactionData[] executedMessages);
+    event BatchCompleted(uint256 batchIndex, TransactionData[] executedMessages);
 
     struct Bid {
         address bidder;
@@ -51,18 +61,134 @@ contract Auction {
         uint256 endBlock = bids[0].startBlock + auctionDuration;
         require(block.number >= endBlock, "Auction not yet ended");
 
-        uint256 winningIndex = 0;
-        uint256 maxScore = 0;
+        uint256[] memory winningIndices;
+        TransactionData[][] memory executedMessages;
 
         for (uint256 i = 0; i < bids.length; i++) {
-            uint256 score = bids[i].bidAmount / bids[i].numBids;
+            if (isUniqueBatch(bids[i].txDataList)) {
+                winningIndices.push(i);
+                executedMessages.push(bids[i].txDataList);
+                continue;
+            }
 
-            if (score > maxScore) {
-                maxScore = score;
-                winningIndex = i;
+            uint256 winnerIndex = i;
+            uint256 loserIndex;
+
+            for (uint256 j = i + 1; j < bids.length; j++) {
+                if (isConflictingBatch(bids[i].txDataList, bids[j].txDataList)) {
+                    uint256 winnerScore = bids[winnerIndex].bidAmount / bids[winnerIndex].numBids;
+                    uint256 contenderScore = bids[j].bidAmount / bids[j].numBids;
+
+                    if (contenderScore > winnerScore) {
+                        loserIndex = winnerIndex;
+                        winnerIndex = j;
+                    } else {
+                        loserIndex = j;
+                    }
+
+                    // Get the new batch containing the remaining messages of the losing bid
+                    TransactionData[] memory newBatch = executeRemainingMessages(bids[winnerIndex].txDataList, bids[loserIndex].txDataList);
+                    
+                    // Add the new batch to the list of bids
+                    if (newBatch.length > 0) {
+                        Bid memory newBid = Bid({
+                            bidder: bids[loserIndex].bidder,
+                            bidAmount: bids[loserIndex].bidAmount,
+                            numBids: newBatch.length,
+                            txDataList: newBatch,
+                            startBlock: block.number
+                        });
+                        bids.push(newBid);
+                    } else {
+                        revert("Loser has no messages to execute");
+                    }
+                }
+            }
+
+            winningIndices.push(winnerIndex);
+            executedMessages.push(bids[winnerIndex].txDataList);
+
+            // Emit the BatchCompleted event for the winning batch
+            emit BatchCompleted(winnerIndex, bids[winnerIndex].txDataList);
+
+            // Emit the BatchCompleted event for the remaining batch (if it exists)
+            if (newBatch.length > 0) {
+                emit BatchCompleted(bids.length - 1, newBatch);
             }
         }
 
-        // Payout logic to message creators and cleanup
+        // ... (Transfer funds and register payouts)
+
+        // Emit the AuctionCompleted event
+        emit AuctionCompleted(winningIndices, executedMessages);
+
+        // Cleanup bids
+        delete bids;
     }
+
+
+    function isUniqueBatch(TransactionData[] memory batch) internal pure returns (bool) {
+        for (uint256 i = 0; i < batch.length; i++) {
+            for (uint256 j = i + 1; j < batch.length; j++) {
+                if (isEqualTransactionData(batch[i], batch[j])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    function isConflictingBatch(TransactionData[] memory batch1, TransactionData[] memory batch2) internal pure returns (bool) {
+        for (uint256 i = 0; i < batch1.length; i++) {
+            for (uint256 j = 0; j < batch2.length; j++) {
+                if (isEqualTransactionData(batch1[i], batch2[j])) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    function isEqualTransactionData(TransactionData memory data1, TransactionData memory data2) internal pure returns (bool) {
+        return (
+            data1.target == data2.target &&
+            data1.amount == data2.amount &&
+            data1.deadline == data2.deadline &&
+            keccak256(data1.data) == keccak256(data2.data)
+        );
+    }
+
+        function executeRemainingMessages(TransactionData[] memory winnerBatch, TransactionData[] memory loserBatch) internal returns (TransactionData[] memory) {
+        TransactionData[] memory newBatch;
+        uint256 newIndex = 0;
+
+        for (uint256 i = 0; i < loserBatch.length; i++) {
+            bool found = false;
+
+            for (uint256 j = 0; j < winnerBatch.length; j++) {
+                if (isEqualTransactionData(loserBatch[i], winnerBatch[j])) {
+                    found = true;
+                    break;
+                }
+            }
+
+            // Add the remaining messages from the loserBatch that are not in the winnerBatch to the newBatch
+            if (!found) {
+                newBatch[newIndex] = loserBatch[i];
+                newIndex++;
+            }
+        }
+
+        return newBatch;
+    }
+
+    function executeBatchesInCustomOrder(IRouter router, bytes32[] memory calldataHashes) public {
+    for (uint256 i = 0; i < calldataHashes.length; i++) {
+        bytes32 calldataHash = calldataHashes[i];
+
+        // Call the external router contract to handle the final target of the calldata
+        router.execute(calldataHash);
+    }
+}
+
 }
